dedd3e14f5fa3a3f4133ab9af2914b01561164ed
Laiba Naheed <laiba.naheedcool123@gmail.com>
2025-07-03 18:08:30 +0500

    Add files via upload

---
+++ C ++ LOOPS/PROGRAM 1.cpp
@@ -0,0 +1,42 @@
+int main ()
+{
+int op; do {
+cout<<"||****||MENU||****||"<<endl; 
+cout<<" OPTION 1 <SIGNUP>"<<endl;
+ cout<<" OPTION 2 <LOGIN>"<<endl;
+  cout<<" OPTION 3 <EXIT>"<<endl;
+cin>>op;
+switch(op) { case 1:
+{
+SIGNUP ();
+break;
+ 
+
+}
+{
+}
+
+} while (op! =3);
+return 0;
+ 
+
+case 2:
+
+
+LOGIN ();
+break;
+
+
+case 3:
+
+
+cout<<"EXITING PROGRAM";
+break;
+
+
+default:
+cout<<"PLEASE SELECT OPTION 1,2,3"<<endl;
+break;}
+ 
+}
+
